3da77d07aa78b27c7aa3fe393d485e5d5361cbba
dmitry <krasnykhda@gmail.com>
2022-11-02 18:35:36 +0300

    ds

---
+++ src/main/java/unitTesting/Testing.java
@@ -0,0 +1,17 @@
+package unitTesting;
+
+/**
+ * @author Manjunath-PC
+ * @created 18/08/2020
+ * @project github-example-package
+ */
+public class Testing {
+
+    public static void display(String param){
+        System.out.println("You parameter is " + param);
+    }
+
+    public void someFunc(){
+        System.out.println("Testing.someFunc");
+    }
+}
